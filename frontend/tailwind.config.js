d0d7e587da156fa21e145a88470e2ff5cc9fd882
ed7moran-ops <ed7moran@gmail.com>
2026-06-09 22:14:44 -0500

    Implement Beer House Palenque app

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        beer: {
+          gold: '#d4af37',
+          amber: '#f7c948',
+          black: '#0b0b0f',
+          charcoal: '#17171f'
+        }
+      }
+    }
+  },
+  plugins: []
+}
